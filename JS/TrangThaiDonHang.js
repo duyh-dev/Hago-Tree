3cb9eb83cbd3fb89fdf59364975d1a3f3a5f3f1a
PALATTTT <85782033+NguyenThanhHai-palat@users.noreply.github.com>
2025-06-23 23:49:26 +0700

    Update TrangThaiDonHang.js

--- JS/TrangThaiDonHang.js
+++ JS/TrangThaiDonHang.js
@@ -47,7 +47,7 @@ setTimeout(() => {
             try {
               const productList = JSON.parse(order.DonHang || "[]");
               products = productList
-                .map(p => `${p.title} (SL: ${p.quantity}, Giá: ${p.price.toLocaleString()}đ)`)
+                .map(p => `${p.title} x ${p.quantity}( Giá: ${p.price.toLocaleString()}đ)`)
                 .join("<br>");
             } catch {
               products = "---";
